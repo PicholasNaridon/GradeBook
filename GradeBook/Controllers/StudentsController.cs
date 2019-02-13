e41bbd9f31f1064a29360f4b754be4a8e56035d1
NickParidon <paridonn1@gmail.com>
2019-02-13 10:43:25 -0500

    New controllers

---
+++ GradeBook/Controllers/StudentsController.cs
@@ -0,0 +1,50 @@
+﻿using GradeBook.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GradeBook.Controllers
+{
+    [Route("api/[controller]")]
+    public class StudentsController : Controller
+    {
+        private readonly GradeBookContext _ctx;
+
+        public StudentsController(GradeBookContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        [Route("[action]")]
+        public IActionResult Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("[action]")]
+        public IActionResult Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("[action]")]
+        public IActionResult FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("[action]")]
+        public IActionResult FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("[action]")]
+        public IActionResult Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GradeBook/Controllers/TeachersController.cs
@@ -0,0 +1,52 @@
+﻿using GradeBook.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GradeBook.Controllers
+{
+    [Route("api/[controller]")]
+    public class TeachersController : Controller
+    {
+        private readonly GradeBookContext _ctx;
+
+        public TeachersController(GradeBookContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        [Route("[action]")]
+        public IActionResult Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("[action]")]
+        public IActionResult Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public IActionResult FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public IActionResult FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("[action]")]
+        public IActionResult Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
